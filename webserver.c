4cbe5c9bfbf664e629e1d46468d1dd958070ca04
kenneth <kenneth@ozymandias.>
2025-02-16 01:58:59 +0700

    add webserver c file

---
+++ webserver.c
@@ -0,0 +1,38 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <errno.h>
+
+#define PORT 80
+
+int main() {
+
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (sockfd < 0) {
+    perror("socket");
+    return 1;
+  }
+
+  printf("socket success\n");
+
+  struct sockaddr_in address;
+  int address_len = sizeof(address);
+
+  address.sin_family = AF_INET;
+  address.sin_port = htons(PORT);
+  address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  if (bind(sockfd, (struct sockaddr *)&address, address_len) < 0) {
+    perror("bind");
+  }
+  printf("bind success\n");
+ 
+  if (listen(sockfd, SOMAXCONN) < 0) {
+    perror("listen");
+  }
+  printf("listening");
+
+  return 0;
+}
